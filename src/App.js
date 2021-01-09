7d247ab6c78630078505a2ad6939f7584e707f1b
KamrulHasaanAnanda <anaada2015@gmail.com>
2021-01-09 12:11:57 +0600

    jsx

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let firstName="Ananda";
+  let lastName="Hassan";
+  let age=23;
+  let job="Intern";
+  let getFullName=(firstName,lastName)=>
+  `${firstName} ${lastName}`
+  let placeholder="Enter your details";
+  let blog={
+    title:"Blog 1",
+    descriptison:"lorm Ipsum dolor lorm Ipsum dolor lorm Ipsum dolor lorm Ipsum dolor "
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{getFullName(firstName,lastName)}</h1>
+      <p>{age}</p>
+      <p>{job}</p>
+      <input placeholder={placeholder} autoComplete></input>
+      <div>
+        <h2>{blog.title}</h2>
+        <p>{blog.descriptison}</p>
+      
+      </div>
+      <hr></hr>
+      <div>
+        <h2>{blog.title}</h2>
+        <p>{blog.descriptison}</p>
+      
+      </div>
     </div>
   );
 }
