7c133aa821f7fecb03c489c06a36ff47dabe3d40
KamrulHasaanAnanda <anaada2015@gmail.com>
2021-01-09 15:36:17 +0600

    css module added

--- src/Blog.js
+++ src/Blog.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {logs} from "./another";
+import classes from './Blog.module.css';
 let blog=(props)=>{
     logs(props);
     return(
-        <div >
+        <div className={classes.style} >
             <h2>{ props.title}</h2>
             <p>{props.description}</p>
           
